8669f49cb71e80c0bf43c36c81f8ac8b76c45aba
Heri RJ <herycrosby@gmail.com>
2026-01-20 12:05:42 +0300

    Le quiz des musiciens

---
+++ music quiz.js
@@ -0,0 +1,23 @@
+function rightAnswer() {
+  const el = document.getElementById("showAnswer")
+  el.innerHTML = "✅ C'est bien la réponse A. Bien joué !";
+ }
+
+function ansTwo() {
+  alert("❌ Oups, mauvaise réponse !");
+  }
+
+function rightAnswerTwo(){
+  const el= document.getElementById("showAnswerTwo");
+  el.innerHTML = "✅ Réponse C. Bravo !";
+}
+
+function rightAnswerThree(){
+  const el=document.getElementById("showAnswerThree");
+  el.innerHTML = "✅Bravo, c'est bien la réponse B."
+}
+
+function rightAnswerFour () {
+  const el = document.getElementById("showAnswerFour");
+  el.innerHTML = "✅Oui, la réponse B est la résolution II-V-I mineur dans Autumn Leaves."
+}
